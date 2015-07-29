e0667abf298c76c74ff85c2abd331bdba847e345
amar gupta <amardeep.gupta@commonfloor.com>
2015-07-30 00:51:01 +0530

    the camera and galary pic image done

--- amar_new/app/src/main/java/ak/amar_new/activities/OutletEditScreen.java
+++ amar_new/app/src/main/java/ak/amar_new/activities/OutletEditScreen.java
@@ -4,15 +4,26 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
 
 import ak.amar_new.R;
 
@@ -25,6 +36,8 @@ public class OutletEditScreen extends ActionBarActivity {
     Menu mMenu;
     private Button submitButton;
     private static final int FILE_SELECT_CODE = 220;
+    private static final int CAMERA_REQUEST = 221;
+    ImageView imageView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +47,8 @@ public class OutletEditScreen extends ActionBarActivity {
         getSupportActionBar().setTitle("Create New Outlet");
         //getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        imageView = (ImageView) findViewById(R.id.outlet_edit_imageView);
+
 
 
     }
@@ -60,6 +75,8 @@ public class OutletEditScreen extends ActionBarActivity {
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
                 Toast.makeText(getApplicationContext(), "camera", Toast.LENGTH_LONG).show();
                 dialog.dismiss();
             }
@@ -92,11 +109,52 @@ public class OutletEditScreen extends ActionBarActivity {
             case FILE_SELECT_CODE:
                 if(resultCode == RESULT_OK)
                 {
+                    String filePath = data.getData().getPath().toString();
+                    Log.i("amar","path = "+filePath);
 
+                    Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                    imageView.setImageBitmap(bitmap);
+                    imageView.setVisibility(View.VISIBLE);
+                    saveImage(bitmap);
                 }
                 break;
+            case CAMERA_REQUEST:
+                if(resultCode==RESULT_OK)
+                {
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    imageView.setImageBitmap(photo);
+                    imageView.setVisibility(View.VISIBLE);
+                    saveImage(photo);
+                }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+    public void saveImage(Bitmap bitmap)
+    {
+        File extFile = Environment.getExternalStorageDirectory();
+        File file = new File(extFile,"/ak_projects");
+        if(!file.exists())
+        {
+            Log.i("amar","Directory does not exists");
+
+            file.mkdir();
+        }
+        int n = 10000;
+        Random generator = new Random();
+        n = generator.nextInt(n);
+        String fName = "ak-"+ n +".jpg";
+        File file1 = new File (file, fName);
+        if (file.exists ()) file1.delete ();
+        try {
+            Log.i("amar","Creating the image file");
+            FileOutputStream out = new FileOutputStream(file1);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
