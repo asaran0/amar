9c27c42899afc03fd486ead1568f422a94f1ff41
amar gupta <amardeep.gupta@commonfloor.com>
2015-08-31 12:51:05 +0530

    changing to mac

--- amar_new/app/src/main/java/ak/amar_new/activities/OutletListScreen.java
+++ amar_new/app/src/main/java/ak/amar_new/activities/OutletListScreen.java
@@ -2,18 +2,40 @@ package ak.amar_new.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import ak.amar_new.R;
 
 /**
  * Created by amar on 26/6/15.
  */
-public class OutletListScreen extends Activity {
-
+public class OutletListScreen extends ActionBarActivity {
+    private Toolbar mToolbar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.outlet_list_screen);
+        mToolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("My Outlets");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+        {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
